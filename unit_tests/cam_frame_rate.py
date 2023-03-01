045d06df9538829e2c371a826dcaa19d75488bdc
linzhanguca <lzhang12@uca.edu>
2023-02-28 22:49:51 -0600

    add frame rate test script

---
+++ unit_tests/cam_frame_rate.py
@@ -0,0 +1,89 @@
+import sys
+import numpy as np
+import pygame
+from pygame.locals import *
+from pygame import event, display, joystick
+from adafruit_servokit import ServoKit
+from gpiozero import PhaseEnableMotor
+import cv2 as cv
+
+from time import time
+
+
+# SETUP
+# init engine and steering wheel
+engine = PhaseEnableMotor(phase=19, enable=26)
+kit = ServoKit(channels=16, address=0x40)
+steer = kit.servo[15]
+MAX_THROTTLE = 0.50
+STEER_CENTER = 95.5
+MAX_STEER = 60
+engine.stop()
+steer.angle = STEER_CENTER
+# init jotstick controller
+display.init()
+joystick.init()
+print(f"{joystick.get_count()} joystick connected")
+js = joystick.Joystick(0)
+# init camera
+cv.startWindowThread()
+cam = cv.VideoCapture(0)
+cam.set(cv.CAP_PROP_FPS, 20)
+# init timer
+start_stamp = time()
+frame_counts = 0
+ave_frame_rate = 0.
+
+
+# MAIN
+try:
+    while True:
+        ret, frame = cam.read()
+        if not ret:  # check camera
+            print("No image received!")
+            engine.stop()
+            engine.close()
+            cv.destroyAllWindows()
+            pygame.quit()
+            sys.exit()
+        for e in event.get():
+            if e.type == QUIT:
+                print("QUIT detected, terminating...")
+                engine.stop()
+                engine.close()
+                cv.destroyAllWindows()
+                pygame.quit()
+                sys.exit()
+            if e.type == JOYAXISMOTION:
+                ax0_val = js.get_axis(0)
+                ax4_val = js.get_axis(4)
+                vel = -np.clip(ax4_val, -MAX_THROTTLE, MAX_THROTTLE)
+                if vel > 0:  # drive motor
+                    engine.forward(vel)
+                elif vel < 0:
+                    engine.backward(-vel)
+                else:
+                    engine.stop()
+                ang = STEER_CENTER + MAX_STEER * ax0_val
+                steer.angle = ang  # drive servo
+                action = (ax0_val, ax4_val)  # steer, throttle
+                # print(f"throttle axis: {ax4_val}, steering axis: {ax0_val}\nengine speed: {vel}, steering angle: {ang}")
+        frame_counts += 1
+        duration_since_start = time() - start_stamp
+        ave_frame_rate = frame_counts / duration_since_start
+        print(f"frame rate: {ave_frame_rate}")
+
+        if cv.waitKey(1) == ord('q'):
+            engine.stop()
+            engine.close()
+            cv.destroyAllWindows()
+            pygame.quit()
+            sys.exit()
+
+except KeyboardInterrupt:
+    engine.stop()
+    engine.close()
+    cv.destroyAllWindows()
+    pygame.quit()
+    sys.exit()
+
